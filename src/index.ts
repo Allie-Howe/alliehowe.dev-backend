c5c60c43276075c60efdfb9840a0c1bb9bbe30d7
Allie Howe <howeisallie@gmail.com>
2023-07-18 17:36:54 +0100

    spotify integration - added lastFM fetch request

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
 import express from 'express';
+import { configDotenv } from 'dotenv';
 
 const app = express();
 const port = process.env.PORT || 4321;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+configDotenv();
+const lastFmUser = 'HoweIsAllie';
+
+app.get('/', async (_, res) => {
+  const lfmRes = await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastFmUser}&api_key=${process.env.LASTFM_CLIENT}&format=json`)
+  if (!lfmRes.ok) throw new Error(lfmRes.statusText);
+
+  const data = JSON.parse(await lfmRes.text());
+
+  const rawMostRecentTrack = data.recenttracks.track[0];
+
+  const mostRecentTrack = mapRawTrackToTrack(rawMostRecentTrack)
+
+  res.send(mostRecentTrack);
 });
 
 app.listen(port, () => {
   return console.log(`Express is listening on port ${port}`);
 });
+
+const mapRawTrackToTrack = (rawTrack: any) => {
+  const rawDate = rawTrack.date?.['#text']
+  return {
+    name: rawTrack.name,
+    artist: rawTrack.artist['#text'],
+    album: rawTrack.album['#text'],
+    image: rawTrack.image[3]['#text'],
+    url: rawTrack.url,
+    date: rawDate ? new Date(rawDate) : undefined,
+    nowPlaying: !!rawTrack['@attr']?.nowplaying,
+  }
+}
